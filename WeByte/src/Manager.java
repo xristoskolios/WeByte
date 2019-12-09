1d2d5d6867a536c57ec69e8a5eabfa26b531c87c
eskountis <lefteris.skountis@gmail.com>
2019-12-09 20:29:19 +0200

    Add toString() and some options.

--- WeByte/src/Manager.java
+++ WeByte/src/Manager.java
@@ -30,83 +30,87 @@ public class Manager extends User {
 		managers.add(this);
 	}
 	
-	/** Having Setters & Getters so it's possible get access to the object's fields.*/
+	/** Having Getters so it's possible get access to the object's fields.*/
 	public String getPassword() {
 		return password;
 	}
-
-	public void setPassword(String password) {
-		this.password = password;
+	/**Method toString so we can see the object of the class Manager.*/
+	public String toString() {
+		return "Manager [name= " + this.getName() + " email= " + this.getEmail() + " pw= " + this.getPassword() + "]";
 	}
-	
-	
 	/** Showing the menu and also getting answer from the user-manager to calculate
 	 * some of the given choices for statistic reasons.
 	 */
 
-	public void getMenu() {
-		System.out.println("Welcome, Mr/Ms." + this.getName() + "\n"
-				+ "Please insert 1 for loyal customers.\n"
-				+ "Please insert 2 for customers who are most likely to be lost.\n"
-				+ "Please insert 3 for the Quality of Mobile Services.\n"
-				+ "Please insert 4 for the Quality of the Internet.\n"
-				+ "Please insert 5 for the Quality of the Satellite TV.\n"
-				+ "Please insert 6 for the Quality of Customer Service.\n"
-				+ "Please insert 7 for Questionnaire evaluation.");
+	public static void getMenu(Manager m) {
 		Scanner sc = new Scanner(System.in);
-		int ans = 0;	//Managers's given answer from the keyboard using class Scanner.
-		boolean b = false;
-		do {
-		try {
+		int ans;	//Managers's given answer from the keyboard using class Scanner.
+		boolean b;
+		while (true) {
+			ans = -1;
+			System.out.println("Welcome, Mr/Ms." + m.getName() + "\n"
+					+ "Please insert 1 for loyal customers.\n"
+					+ "Please insert 2 for customers who are most likely to be lost.\n"
+					+ "Please insert 3 for the Quality of Mobile Services.\n"
+					+ "Please insert 4 for the Quality of the Internet.\n"
+					+ "Please insert 5 for the Quality of the Satellite TV.\n"
+					+ "Please insert 6 for the Quality of Customer Service.\n"
+					+ "Please insert 7 for Questionnaire evaluation.\n"
+					+ "Please insert 0 to get back to the Starting Menu");
+			b = false;
 			do {
-				ans = sc.nextInt();
-				if (!(ans >= 1 && ans <= 7)) {
-				System.err.println("Wrong input.Please insert one of the choices.");
+				try {
+					do {
+						ans = sc.nextInt();
+						if (!(ans >= 0 && ans <= 7)) {
+							System.err.println("Wrong input.Please insert one of the choices.");
+						}
+					} while (ans >= 0 && ans <= 7);
+					b = true;
+				} catch (InputMismatchException e){
+					System.err.println("Please type an integer.\nTry again.");
+					sc.nextLine(); //Clearing the scanner if Manager gives String instead of int.
 				}
-			} while (ans >= 1 && ans <= 7);
-			b = true;
-		} catch (InputMismatchException e){
-			System.err.println("Please type an integer.\nTry again.");
-			sc.nextLine(); //Clearing the scanner if Manager gives string instead of int.
+			} while (b == false);
+			if (ans == 1) {
+				//method FIXME
+			} else if (ans == 2) {
+				//method FIXME
+			} else if (ans == 3) {
+				getMobileQResults();
+			} else if (ans == 4) {
+				getInternetQResults();
+			} else if (ans == 5) {
+				getSateliteTVQResults();
+			} else if (ans == 6) {
+				//method FIXME
+			} else if (ans == 7) {
+				//method FIXME
+			} else {//ans == 0
+				WeByte.getStartingMenu();
+			}
 		}
-		} while (b == false);
-		if (ans == 1) {
-			//method FIXME
-		} else if (ans == 2) {
-			//method FIXME
-		} else if (ans == 3) {
-			getQ2Results();
-		} else if (ans == 4) {
-			getInternetQResults();
-		} else if (ans == 5) {
-			getSateliteTVQResults();
-		} else if (ans == 6) {
-			//method FIXME
-		} else if (ans == 7) {
-			//method FIXME
-		}
-		sc.close();
 	}
 	
 	/* Log-in method for the Managers.*/
-	public void login() {
-		System.out.println("~~~LOGIN AS MANAGER~~~");
+	public static void login() {
 		while (true) {
+			System.out.println("~~~LOGIN AS MANAGER~~~");
 			Scanner in = new Scanner(System.in);
 			System.out.println("Please insert your email.");
 			String em = in.nextLine();
-			if (em == "\n" ) {
-				Webyte.startingMenu();
+			if (em.equals("")) {
+				WeByte.getStartingMenu();
 			}
 			System.out.println("Please insert your password.");
 			String pw = in.nextLine();
-			if (pw == "\n") {
-				Webyte.startingMenu();
+			if (pw.equals("")) {
+				WeByte.getStartingMenu();
 			}
 			for (Manager i: managers) {
 				if (em.equals(i.getEmail()) && pw.equals(i.getPassword())) {
-					System.out.println("Log in done successfully.");
-					this.getMenu();
+					System.out.println("Log in done successfully.\nWelcome, Mr/Ms." + i.getName() + "\n");
+					getMenu(i);
 				}	
 			}
 			System.out.println("Wrong email or/and password.Please try again.");
@@ -114,11 +118,11 @@ public class Manager extends User {
 	}
 	
 	//** Method to calculate the Quality of Mobile Services from answer sheet(ArrayList) from Questionnaire's field q2.*/
-	public static void getQ2Results() {
+	public static void getMobileQResults() {
 		System.out.println("---The Quality of Mobile Services---");
-		int a1 = 0,a2 = 0,a3 = 0,a4 = 0,a5 = 0; //ANSWERS 1,2,3,4,5 FROM QUESTION 2 OF THE QUESTIONNAIRE
+		int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0; //ANSWERS 1,2,3,4,5 FROM QUESTION 2 OF THE QUESTIONNAIRE
 		for (Questionnaire k: Questionnaire.Qs) {
-			if (k.q2 == 1) {
+			if (k.q1 == 1) {//FIXME k.getq1(); q1 is private.
 				a1++;
 			} else if (k.q2 == 2) {
 				a2++;
@@ -131,13 +135,13 @@ public class Manager extends User {
 			}
 		}
 		int n = Questionnaire.n; //Number of questionnaires done.
-		//Otherwise int n = Questionnaire.Qs.length;
+		//Otherwise int n = Questionnaire.Qs.length();
 		if (n>0) {
-			double p1 = (double) 100*a1/n; //Rate of answer 1
-			double p2 = (double) 100*a2/n; //Rate of answer 2
-			double p3 = (double) 100*a3/n; //Rate of answer 3
-			double p4 = (double) 100*a4/n; //Rate of answer 4
-			double p5 = (double) 100*a5/n; //Rate of answer 5
+			double p1 = (double) 100 * a1 / n; //Rate of answer 1
+			double p2 = (double) 100 * a2 / n; //Rate of answer 2
+			double p3 = (double) 100 * a3 / n; //Rate of answer 3
+			double p4 = (double) 100 * a4 / n; //Rate of answer 4
+			double p5 = (double) 100 * a5 / n; //Rate of answer 5
 		System.out.printf("The rate of asnwer 1 is: %.2f%%" + p1 + ".\n");
 		System.out.printf("The rate of asnwer 2 is: %.2f%%" + p2 + ".\n");
 		System.out.printf("The rate of asnwer 3 is: %.2f%%" + p3 + ".\n");
@@ -150,7 +154,7 @@ public class Manager extends User {
 	//** Method to calculate the Quality of the Internet from answer sheet(ArrayList) from Questionnaire's field q3.*/
 	public static void getInternetQResults() {
 		System.out.println("---The Quality of the Internet---");
-		int a1 = 0,a2 = 0,a3 = 0,a4 = 0,a5 = 0; //ANSWERS 1,2,3,4,5 FROM QUESTION 3 OF THE QUESTIONNAIRE
+		int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0; //ANSWERS 1,2,3,4,5 FROM QUESTION 3 OF THE QUESTIONNAIRE
 		for (Questionnaire k: Questionnaire.Qs) {
 			if (k.q3 == 1) {
 				a1++;
@@ -166,11 +170,11 @@ public class Manager extends User {
 		}
 		int n = Questionnaire.n; //Number of questionnaires done.
 		if (n>0) {
-			double p1 = (double) 100*a1/n; //Rate of answer 1
-			double p2 = (double) 100*a2/n; //Rate of answer 2
-			double p3 = (double) 100*a3/n; //Rate of answer 3
-			double p4 = (double) 100*a4/n; //Rate of answer 4
-			double p5 = (double) 100*a5/n; //Rate of answer 5
+			double p1 = (double) 100 * a1 / n; //Rate of answer 1
+			double p2 = (double) 100 * a2 / n; //Rate of answer 2
+			double p3 = (double) 100 * a3 / n; //Rate of answer 3
+			double p4 = (double) 100 * a4 / n; //Rate of answer 4
+			double p5 = (double) 100 * a5 / n; //Rate of answer 5
 			System.out.printf("The rate of answer 1 is: %.2f%%" + p1 + ".\n");
 			System.out.printf("The rate of answer 2 is: %.2f%%" + p2 + ".\n");
 			System.out.printf("The rate of answer 3 is: %.2f%%" + p3 + ".\n");
@@ -183,7 +187,7 @@ public class Manager extends User {
 	//** Method to calculate the Quality of the Satellite TV from answer sheet(ArrayList) from Questionnaire's field q4.*/
 	public static void getSateliteTVQResults() {
 		System.out.println("---The Quality of the Satellite TV---");
-		int a1 = 0,a2 = 0,a3 = 0,a4 = 0,a5 = 0; //ANSWERS 1,2,3,4,5 FROM QUESTION 4 OF THE QUESTIONNAIRE
+		int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0; //ANSWERS 1,2,3,4,5 FROM QUESTION 4 OF THE QUESTIONNAIRE
 		for (Questionnaire k: Questionnaire.Qs) {
 			if (k.q4 == 1) {
 				a1++;
@@ -197,13 +201,13 @@ public class Manager extends User {
 				a5++;
 			}
 		}
-		int n = Questionnaire.n; //Number of questionnaires done.
+		int n = Questionnaire.n; //Number of Questionnaires done.
 		if (n>0) {
-			double p1 = (double) 100*a1/n; //Rate of answer 1
-			double p2 = (double) 100*a2/n; //Rate of answer 2
-			double p3 = (double) 100*a3/n; //Rate of answer 3
-			double p4 = (double) 100*a4/n; //Rate of answer 4
-			double p5 = (double) 100*a5/n; //Rate of answer 5
+			double p1 = (double) 100 * a1 / n; //Rate of answer 1
+			double p2 = (double) 100 * a2 / n; //Rate of answer 2
+			double p3 = (double) 100 * a3 / n; //Rate of answer 3
+			double p4 = (double) 100 * a4 / n; //Rate of answer 4
+			double p5 = (double) 100 * a5 / n; //Rate of answer 5
 			System.out.printf("The rate of answer 1 is: %.2f%%" + p1 + ".\n");
 			System.out.printf("The rate of answer 2 is: %.2f%%" + p2 + ".\n");
 			System.out.printf("The rate of answer 3 is: %.2f%%" + p3 + ".\n");
