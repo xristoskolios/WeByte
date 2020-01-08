3d33b231937bb73f776c2a91712f40d608d0df23
eskountis <lefteris.skountis@gmail.com>
2020-01-08 13:05:18 +0200

    Fixed Exceptions while running main.

--- WeByte/src/Manager.java
+++ WeByte/src/Manager.java
@@ -65,14 +65,13 @@ public class Manager extends User {
 						if (!(ans >= 0 && ans <= 7)) {
 							System.err.println("Wrong input.Please insert one of the choices.");
 						}
-					} while (ans >= 0 && ans <= 7);
+					} while (!(ans >= 0 && ans <= 7));
 					b = true;
 				} catch (InputMismatchException e) {
 					System.err.println("Please type an integer.\nTry again.");
 					sc.nextLine(); //Clearing the scanner if Manager gives String instead of int.
 				}
 			} while (b == false);
-			sc.close();
 			if (ans == 1) {
 				getLoyalQResults();
 			} else if (ans == 2) {
@@ -111,7 +110,6 @@ public class Manager extends User {
 			for (Manager i: managers) {
 				if (em.equals(i.getEmail()) && pw.equals(i.getPassword())) {
 					System.out.println("Log in done successfully.\nWelcome, Mr/Ms." + i.getName() + "\n");
-					sc.close();
 					getMenu(i);
 				}
 			}
@@ -121,16 +119,24 @@ public class Manager extends User {
 	
 	/** Method to print the loyal customers*/
 		public static void getLoyalQResults() {
-			System.out.println("---The Loyal Customers---");
-			for(int i = 0; i <= Customer.LoyalCustomers.size(); i++) {
-				System.out.println(Customer.LoyalCustomers.get(i).toString()); 		
+			if (Customer.LoyalCustomers.size() == 0) {
+				System.out.println("No loyal Customers found.");
+			} else {
+				System.out.println("---The Loyal Customers---");
+				for(int i = 0; i < Customer.LoyalCustomers.size(); i++) {
+					System.out.println(Customer.LoyalCustomers.get(i).toString()); 		
+				}
 			}
 		}
 	/** Method to print the customers who are most likely to be lost*/
 		public static void getLossQResults() {
-			System.out.println("---The Lost Customers---");
-			for(int i = 0; i <= Customer.LossCustomers.size(); i++) {
-				System.out.println(Customer.LossCustomers.get(i).toString()); 		
+			if (Customer.LossCustomers.size() == 0) {
+				System.out.println("No loss Customers found.");
+			} else {
+				System.out.println("---The Lost Customers---");
+				for(int i = 0; i < Customer.LossCustomers.size(); i++) {
+					System.out.println(Customer.LossCustomers.get(i).toString()); 		
+				}
 			}
 		}
 	/** Method to calculate the Quality of Mobile Services from answer sheet(ArrayList) from Questionnaire's field q2.*/
@@ -146,7 +152,7 @@ public class Manager extends User {
 				a3++;
 			} else if (k.getQ1() == 4) {
 				a4++;
-			} else { // k.q2 == 5, definately q2 must be either 1 either 2 either 3 either 4 either 5.
+			} else { // k.q2 == 5
 				a5++;
 			}
 		}
@@ -158,11 +164,11 @@ public class Manager extends User {
 			double p3 = (double) 100 * a3 / n; //Rate of answer 3
 			double p4 = (double) 100 * a4 / n; //Rate of answer 4
 			double p5 = (double) 100 * a5 / n; //Rate of answer 5
-		System.out.printf("The rate of asnwer 1 is: %.2f%%" + p1 + ".\n");
-		System.out.printf("The rate of asnwer 2 is: %.2f%%" + p2 + ".\n");
-		System.out.printf("The rate of asnwer 3 is: %.2f%%" + p3 + ".\n");
-		System.out.printf("The rate of asnwer 4 is: %.2f%%" + p4 + ".\n");
-		System.out.printf("The rate of asnwer 5 is: %.2f%%" + p5 + ".\n");
+		System.out.printf("The rate of asnwer 1 is: %.2f%%.\n",p1);
+		System.out.printf("The rate of asnwer 2 is: %.2f%%.\n",p2);
+		System.out.printf("The rate of asnwer 3 is: %.2f%%.\n",p3);
+		System.out.printf("The rate of asnwer 4 is: %.2f%%.\n",p4);
+		System.out.printf("The rate of asnwer 5 is: %.2f%%.\n",p5);
 		} else { //No questionnaires done by any Customer.
 			System.out.println("No questionnaires have been done yet.");
 		}
@@ -192,11 +198,11 @@ public class Manager extends User {
 			double p3 = (double) 100 * a3 / n; //Rate of answer 3
 			double p4 = (double) 100 * a4 / n; //Rate of answer 4
 			double p5 = (double) 100 * a5 / n; //Rate of answer 5
-			System.out.printf("The rate of answer 1 is: %.2f%%" + p1 + ".\n");
-			System.out.printf("The rate of answer 2 is: %.2f%%" + p2 + ".\n");
-			System.out.printf("The rate of answer 3 is: %.2f%%" + p3 + ".\n");
-			System.out.printf("The rate of answer 4 is: %.2f%%" + p4 + ".\n");
-			System.out.printf("The rate of answer 5 is: %.2f%%" + p5 + ".\n");
+			System.out.printf("The rate of asnwer 1 is: %.2f%%.\n",p1);
+			System.out.printf("The rate of asnwer 2 is: %.2f%%.\n",p2);
+			System.out.printf("The rate of asnwer 3 is: %.2f%%.\n",p3);
+			System.out.printf("The rate of asnwer 4 is: %.2f%%.\n",p4);
+			System.out.printf("The rate of asnwer 5 is: %.2f%%.\n",p5);
 		} else { //No questionnaires done by any Customer.
 			System.out.println("No questionnaires have been done yet.");
 		}
@@ -226,11 +232,11 @@ public class Manager extends User {
 			double p3 = (double) 100 * a3 / n; //Rate of answer 3
 			double p4 = (double) 100 * a4 / n; //Rate of answer 4
 			double p5 = (double) 100 * a5 / n; //Rate of answer 5
-			System.out.printf("The rate of answer 1 is: %.2f%%" + p1 + ".\n");
-			System.out.printf("The rate of answer 2 is: %.2f%%" + p2 + ".\n");
-			System.out.printf("The rate of answer 3 is: %.2f%%" + p3 + ".\n");
-			System.out.printf("The rate of answer 4 is: %.2f%%" + p4 + ".\n");
-			System.out.printf("The rate of answer 5 is: %.2f%%" + p5 + ".\n");
+			System.out.printf("The rate of asnwer 1 is: %.2f%%.\n",p1);
+			System.out.printf("The rate of asnwer 2 is: %.2f%%.\n",p2);
+			System.out.printf("The rate of asnwer 3 is: %.2f%%.\n",p3);
+			System.out.printf("The rate of asnwer 4 is: %.2f%%.\n",p4);
+			System.out.printf("The rate of asnwer 5 is: %.2f%%.\n",p5);
 		} else { //No questionnaires done by any Customer.
 			System.out.println("No questionnaires have been done yet.");
 		}
@@ -260,11 +266,11 @@ public class Manager extends User {
 			double p3 = (double) 100 * a3 / n; //Rate of answer 3
 			double p4 = (double) 100 * a4 / n; //Rate of answer 4
 			double p5 = (double) 100 * a5 / n; //Rate of answer 5
-			System.out.printf("The rate of asnwer 1 is: %.2f%%" + p1 + ".\n");
-			System.out.printf("The rate of asnwer 2 is: %.2f%%" + p2 + ".\n");
-			System.out.printf("The rate of asnwer 3 is: %.2f%%" + p3 + ".\n");
-			System.out.printf("The rate of asnwer 4 is: %.2f%%" + p4 + ".\n");
-			System.out.printf("The rate of asnwer 5 is: %.2f%%" + p5 + ".\n");
+			System.out.printf("The rate of asnwer 1 is: %.2f%%.\n",p1);
+			System.out.printf("The rate of asnwer 2 is: %.2f%%.\n",p2);
+			System.out.printf("The rate of asnwer 3 is: %.2f%%.\n",p3);
+			System.out.printf("The rate of asnwer 4 is: %.2f%%.\n",p4);
+			System.out.printf("The rate of asnwer 5 is: %.2f%%.\n",p5);
 		} else { //No questionnaires done by any Customer.
 			System.out.println("No questionnaires have been done yet.");
 		}
@@ -285,8 +291,8 @@ public class Manager extends User {
 		if (n>0) {
 			double p1 = (double) 100 * a1 / n; //Rate of answer 1
 			double p2 = (double) 100 * a2 / n; //Rate of answer 2
-		System.out.printf("The rate of asnwer 1 is: %.2f%%" + p1 + ".\n");
-		System.out.printf("The rate of asnwer 2 is: %.2f%%" + p2 + ".\n");
+		System.out.printf("The rate of asnwer 1 is: %.2f%%.\n",p1);
+		System.out.printf("The rate of asnwer 2 is: %.2f%%.\n",p2);
 		} else { //No questionnaires done by any Customer.
 			System.out.println("No questionnaires have been done yet.");
 		}
